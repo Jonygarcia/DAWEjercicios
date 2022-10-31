ee5c82cff9076f2ab303c4bbc68f47539bdb8020
Jonatan García Barrera <jonatangarciabarrera@gmail.com>
2022-10-31 14:09:07 +0100

    10 completado

--- DWEntornoCliente/010comparaciones/010comparaciones.js
+++ DWEntornoCliente/010comparaciones/010comparaciones.js
@@ -12,6 +12,6 @@ alert(5 > 4); // true -> 5 es mayor que 4
 alert("apple" > "microsoft"); // false -> el valor del primer caracter de apple es menor que el de microsoft
 alert("2" > "12"); // true -> el valor del primer y único caracter de 2 es mayor que el primer caracter de 12
 alert(undefined == null); // true -> undefined se convierte en NaN y null en 0, son iguales a la hora de comparar, no tienen valor
-alert(undefined === null); // false -> siguiendo la explicación del anterior, son iguales en comparación pero no idénticos
+alert(undefined === null); // false -> siguiendo la explicación del anterior, son iguales en comparación pero no idénticos ya que no son el mismo tipo
 alert(null == "\n0\n"); // El string con saltos de línea tiene el valor numérico 0, pero null sólo toma el valor numérico 0 en caso de comparar con simbolos <>
 alert(null === +"\n0\n"); // Null no es exactamente igual que el valor numérico 0, además de que null no toma el valor numérico 0 ya que no lo está comparando con 0
